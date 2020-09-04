3b7998675ffbc4a56b036a7424b5912fd23f299e
Karen Perez <karenp.flores47@gmail.com>
2020-09-04 00:49:44 -0500

    auth process complete

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var app = express();
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+})
+.catch((err) => {
+    console.log("Error connecting to mongo", err)
+});
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const authRouter = require('./routes/auth');
+app.use('/', authRouter);
 
 module.exports = app;
