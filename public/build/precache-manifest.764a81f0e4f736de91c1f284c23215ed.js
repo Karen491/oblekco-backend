5838fc71489afaf2caeb0eaae0df4599990f5687
Karen Perez <karenp.flores47@gmail.com>
2020-09-07 22:52:32 -0500

    signup fixed

---
+++ public/build/precache-manifest.764a81f0e4f736de91c1f284c23215ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f408ee18d3ba0c97408e24d9241b51c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34cfaeaff701b1387d11",
+    "url": "/static/css/2.485194a1.chunk.css"
+  },
+  {
+    "revision": "de4ec805ca620a46ba66",
+    "url": "/static/css/main.a03a4fe1.chunk.css"
+  },
+  {
+    "revision": "34cfaeaff701b1387d11",
+    "url": "/static/js/2.c6cd88db.chunk.js"
+  },
+  {
+    "revision": "5c1194be04646d6ba02d2a388afc277e",
+    "url": "/static/js/2.c6cd88db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4ec805ca620a46ba66",
+    "url": "/static/js/main.5fba8da9.chunk.js"
+  },
+  {
+    "revision": "66362ca12fe8dd245cc2",
+    "url": "/static/js/runtime-main.71d0233e.js"
+  }
+]);
