ca93a75179847bfea78ec7de09bee7ce4d0486e8
Karen Perez <karenp.flores47@gmail.com>
2020-09-07 21:55:27 -0500

    home route complete

---
+++ public/build/precache-manifest.5a91c143ede5f240942adb01f2b3eedc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0c3802ba7eb1fc441f895d58ac9f0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34cfaeaff701b1387d11",
+    "url": "/static/css/2.485194a1.chunk.css"
+  },
+  {
+    "revision": "bbd95a7edc1286a44ae8",
+    "url": "/static/css/main.a03a4fe1.chunk.css"
+  },
+  {
+    "revision": "34cfaeaff701b1387d11",
+    "url": "/static/js/2.c6cd88db.chunk.js"
+  },
+  {
+    "revision": "5c1194be04646d6ba02d2a388afc277e",
+    "url": "/static/js/2.c6cd88db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd95a7edc1286a44ae8",
+    "url": "/static/js/main.38a59d6a.chunk.js"
+  },
+  {
+    "revision": "66362ca12fe8dd245cc2",
+    "url": "/static/js/runtime-main.71d0233e.js"
+  }
+]);
